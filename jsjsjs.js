7248345741c66a34be0cf6fe3a666ccbd9993597
azizromdhani7 <rromdhanimmohamedaziz@gmail.com>
2024-09-09 22:08:32 +0100

    done1.2

---
+++ jsjsjs.js
@@ -0,0 +1,14 @@
+$("#bes").append(`
+    <div id="LEKBIRR">
+    <div class="lekbira">
+    <input type="text" placeholder="first name" class="aaz"><br>
+    <input type="text" placeholder="last name" class="aaz"><br>
+    <input type="email" class="azz" id="nn" placeholder="your email"><br>
+    <input type="text" placeholder="experience" class="aaz"><br>
+    <input type="text" placeholder="education" class="aaz"><br>
+    <input type="text" placeholder="other" class="aaz"><br>
+    <input type="text" placeholder="phone number" class="aaz"><br>
+    </div>
+    <div class="thenya"></div>
+    </div>
+`);
